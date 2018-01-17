080b37b80ed2ba78a86e30b4377dc1f6eb1b3a3b
shin <funny.suo@gmail.com>
2018-01-17 14:22:37 +0900

    no message

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@ $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
 
 foreach ($files1 as $key => $value) {
-    if($value == "index.php" || $value == "README.TXT" ||$value == "." ||$value == "sbadmin2-1.0.7"||$value == ".."||$value == ".DS_Store"||$value == "menu.php"||$value == "content.php"){
+    if($value == "index.php" || $value == "README.TXT" ||$value == "." ||$value == "sbadmin2-1.0.7"||$value == ".."||$value == ".DS_Store"||$value == "menu.php"||$value == "content.php"||$value == ".git"){
         continue;
     }
 ?>
